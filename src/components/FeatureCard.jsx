9682ce1528df8981c9e2c0b4c5fe678dc69411c5
ragavi-as <ragaviragi08@gmail.com>
2026-02-16 11:05:39 +0530

    enhanced feture cards

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,16 +1,4 @@
-/*const FeatureCard = ({ title, desc }) => {
-  return (
-    <div className="bg-white p-6 rounded-xl shadow-sm border hover:shadow-md transition">
-      <h3 className="text-lg font-semibold mb-3 text-gray-800">
-        {title}
-      </h3>
-      <p className="text-gray-600 text-sm">{desc}</p>
-    </div>
-  );
-};
 
-export default FeatureCard;
-*/
 const FeatureCard = ({ title, desc, icon }) => {
   return (
     <div className="group relative overflow-hidden rounded-2xl border border-gray-100 bg-white p-8 text-center transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl">
